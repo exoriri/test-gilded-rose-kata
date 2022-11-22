d1338998143154276507ff43136607c48502b63c
exoriri <lavrikrom2@gmail.com>
2022-11-22 13:52:01 +0300

    feat: rewrited gilded rose

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,49 +19,40 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
+            this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (this.items[i].quality < 50 && this.items[i].quality > 0) {
+                switch (this.items[i].name) {
+                    case 'Aged Brie': {
+                        this.items[i].quality = this.items[i].quality + 1;
+                        break;
+                    }
+
+                    case 'Backstage passes to a TAFKAL80ETC concert': {
+                        if (this.items[i].sellIn <= 10) {
+                            this.items[i].quality = this.items[i].quality + 2
+                        }
+                        if (this.items[i].sellIn <= 5) {
+                            this.items[i].quality = this.items[i].quality + 3
+                        };
+                        break;
+                    }
+
+                    case 'Sulfuras, Hand of Ragnaros': {
+                        if (this.items[i].quality !== 80) {
+                            this.items[i].quality = this.items[i].quality + 1;
+                            break;
+                        };
+                        break;
+                    }
+                    default: {
+                        break;
                     }
                 }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
+            };
+
+            if (this.items[i].sellIn < 0 && this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+                this.items[i].quality = 0;
+            };
         }
 
         return this.items;
--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,12 +1,18 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { assert, expect } from "chai";
+import { Item, GildedRose } from "../app/gilded-rose";
 
-describe('Gilded Rose', function () {
+describe("Gilded Rose", function () {
+  it("should handle edge cases logic errors", function () {
+    const gildedRose = new GildedRose(
+        [
+            new Item("Quality", 0, 0),
+        ], 
+    );
+    const items: Item[] = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0, 'Expect quantity of first item not be changed');
+    for (let i = 0; i < items.length; i++) {
+      assert.isAtLeast(items[i].quality, 0, `Item name: ${items[i].name}`);
+    }
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
-
+  });
 });
